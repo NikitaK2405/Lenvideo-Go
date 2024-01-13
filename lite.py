386a0d219623528bcbedc781da2a970c45ef29bd
Никита Караченцев <107871254+NikitaK2405@users.noreply.github.com>
2024-01-13 10:53:18 +0500

    Update lite.py

--- lite.py
+++ lite.py
@@ -11,7 +11,7 @@ init()
 
 def get_video_len(path_directory):
     if os.path.exists(path_directory):
-        print(Fore.CYAN + "[+] Сканирование директории")
+        print(Fore.CYAN + "[+] Directory scanning")
         clip_in_dir = os.listdir(path_directory)
         clip_to_merge = []
         for clip in clip_in_dir:
@@ -20,33 +20,33 @@ def get_video_len(path_directory):
                 clip_to_merge.append(VideoFileClip(os.path.join(path_directory, clip)))
 
         if len(clip_to_merge) <= 1:
-            print(Fore.RED + "[-] В указанной директории нет видеофайлов")
+            print(Fore.RED + "[-] There are no video files in directory")
             main()
             return
         else:
-            print(Fore.YELLOW + f"[+] Найдено файлов: {len(clip_to_merge)}")
+            print(Fore.YELLOW + f"[+] Files found: {len(clip_to_merge)}")
             merge_final = concatenate_videoclips(clip_to_merge)
-            print(Fore.YELLOW + f"[+] Общая длительность найденных видеофайлов: "
+            print(Fore.YELLOW + f"[+] Total duration of found video files: "
                                 f"{time.strftime('%H:%M:%S', time.gmtime(merge_final.duration))}")
             main()
             return
     else:
-        print(Fore.RED + "[-] Указанного пути не существует")
+        print(Fore.RED + "[-] The specified path does not exist")
         main()
         return
 
 
 def main():
-    user_change = input(Fore.LIGHTWHITE_EX + "\nВыберите действие:\n"
-                                             "   [0] Выход\n"
-                                             "   [1] Найти общую продолжительность видеофайлов в папке\n"
+    user_change = input(Fore.LIGHTWHITE_EX + "\nSelect action:\n"
+                                             "   [0] Exit\n"
+                                             "   [1] Find total duration of video files in a folder\n"
                                              "   >>> ")
     if user_change == "0":
         sys.exit()
     elif user_change == "1":
-        get_video_len(input(Fore.LIGHTWHITE_EX + "\n[+] Введите путь к папке с файлами: "))
+        get_video_len(input(Fore.LIGHTWHITE_EX + "\n[+] Enter path to the file folder: "))
     else:
-        print(Fore.RED + "\n[-] Неопознанное действие: повторите попытку")
+        print(Fore.RED + "\n[-] Unidentified action: please try again")
         main()
 
 
